290ba85623930a1f0dc7a1abae1aa6974ad42d16
Hani <jhe93@naver.com>
2022-09-10 15:21:44 +0900

    입력받은 문장을 변수에 저장

--- app/src/main/java/com/metacoding/uikotlinpractice_hani/MainActivity.kt
+++ app/src/main/java/com/metacoding/uikotlinpractice_hani/MainActivity.kt
@@ -2,11 +2,25 @@ package com.metacoding.uikotlinpractice_hani
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //버튼이 눌린 경우 이벤트 달아주기
+        okBtn.setOnClickListener {
+
+            //실행할 이벤트
+            //1. 입력된 내용을 변수에 저장하기(get)
+            val inputContent = contentEdt.text.toString()//string으로 변환 후 저장
+
+            //2. 저장해둔 입력값을 => 텍스트뷰의 내용으로 반영(set)
+            resultTxt.text = inputContent
+
+        }
+
+
     }
-}
